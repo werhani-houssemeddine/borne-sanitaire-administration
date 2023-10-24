8caf926fc5c2c6711e2e39e66854e8e5a9e0a240
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-10-24 15:56:18 +0100

    separate send and submit functions from each other

--- lib/widgets/authentication/Login/login.service.dart
+++ lib/widgets/authentication/Login/login.service.dart
@@ -1,20 +1,45 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<String> submitForm(String email, String password) async {
-  var client = http.Client();
-  var response = await client.post(
-      Uri.http('localhost:8000', '/administration/login/'),
-      body: {'email': email, 'password': password});
+Future _sentFormData(Object? body) async {
+  try {
+    var client = http.Client();
+    Uri serverURL = Uri.http('localhost:8000', '/administration/login/');
+    return client.post(serverURL, body: body);
+  } catch (e) {
+    return null;
+  }
+}
 
-  print(response.body);
-  if (response.statusCode == 200) {
-    print('data sent successfully');
+Future<String> submitForm(String email, String password) async {
+  var loginResponse =
+      await _sentFormData({'email': email, 'password': password});
+
+  /*if (loginResponse == null) {
+    return "SERVER UNREACHED";
+  }*/
+
+  //! This will be represented in a class in future version
+  int statusCode = loginResponse.statusCode;
+  final data = json.decode(loginResponse.body);
+  var error = data["error"];
+  var state = data["state"];
+  var message = data["message"];
+
+  if (statusCode >= 200 && statusCode < 300) {
+    //! the logic behind this API Is to send error and state to indicate
+    //! if the request is made properly [error refers that the request in not valid]
+    //! and state will indicate the if the request made successfully
+    if (error == false && state == "failure") {
+      return "FAILURE";
+    } else if (error == false && state == "success") {
+      return "SUCCESS";
+    }
+
+    return "UNKNOWN ERROR";
+  } else if (statusCode >= 500) {
+    return "INTERNAL SERVER ERROR";
   } else {
-    print('error occurred while sending data to server');
+    return message; // "BAD REQUEST"
   }
-  if (response.body ==
-      'Authentication done and verification code send successfully') {
-    print('dooooone');
-  }
-  return response.body;
 }
