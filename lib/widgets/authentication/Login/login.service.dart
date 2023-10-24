1461dafcf44c8841f3fb3c1c77bd8ab3630c304f
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-10-24 14:25:18 +0100

    login service

---
+++ lib/widgets/authentication/Login/login.service.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<String> submitForm(
+    TextEditingController email, TextEditingController password) async {
+  var client = http.Client();
+  var response = await client.post(
+      Uri.http('localhost:8000', '/administration/login/'),
+      body: {'email': email, 'password': password});
+
+  print(response.body);
+  if (response.statusCode == 200) {
+    print('data sent successfully');
+  } else {
+    print('error occurred while sending data to server');
+  }
+  if (response.body ==
+      'Authentication done and verification code send successfully') {
+    print('dooooone');
+  }
+  return response.body;
+}
