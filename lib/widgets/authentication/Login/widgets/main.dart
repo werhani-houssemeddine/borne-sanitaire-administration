72dd098ff1810af79fc2757063746e30a6da6262
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-10-27 08:36:46 +0100

    add stack widget (need to update)

--- lib/widgets/authentication/Login/widgets/main.dart
+++ lib/widgets/authentication/Login/widgets/main.dart
@@ -5,7 +5,31 @@ import 'package:flutter/material.dart';
 
 class LoginWidgets {
   static Widget Login() {
-    return const LoginForm();
+    //! Need to update make
+    //! Make the stack inside a container (for make a good positions)
+    return Stack(
+      alignment: Alignment.center,
+      //padding: EdgeInsets.all(20.0),
+      children: [
+        const LoginForm(),
+        Positioned(
+            top: 40,
+            child: Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(112.0),
+                boxShadow: kElevationToShadow[4],
+              ),
+              child: Icon(Icons.security,
+                  size: 124.0,
+                  color: Colors
+                      .greenAccent.shade400 //Color.fromARGB(255, 43, 249, 74),
+                  ),
+            )),
+      ],
+    );
   }
 
   static Widget EmailInput(TextEditingController controller) {
