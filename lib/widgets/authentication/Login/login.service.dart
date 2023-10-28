e43ae3a57ffbe2d9efae51c845454fc270a9bb0b
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-10-28 22:17:32 +0100

    code refactored and need for more refactoring

--- lib/widgets/authentication/Login/login.service.dart
+++ lib/widgets/authentication/Login/login.service.dart
@@ -1,45 +1,79 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future _sentFormData(Object? body) async {
+Future<String> submitForm(String email, String password) async {
   try {
-    var client = http.Client();
-    Uri serverURL = Uri.http('localhost:8000', '/administration/login/');
-    return client.post(serverURL, body: body);
+    var loginResponse = await _LoginService.submitCredentials(email, password);
+    var response = _LoginService.extractResponse(loginResponse);
+
+    if (response['statusCode'] >= 200 && response['statusCode'] < 300) {
+      if (response['state'] == 'FAILURE') {
+        //! Return WRONG CREDENTIALS
+        return response['message'];
+      } else {
+        //! Return SUCCESS
+        return response['state'];
+      }
+    } else if (response['statusCode'] < 500) {
+      if (response['message'] == "BAD REQUEST") {
+        return response['details']; //! Return the error details
+      }
+      //! To handle other error format (Unauthorised ...)
+      return response['details'] ?? response['message'];
+    } else {
+      return "INTERNAL SERVER ERROR";
+    }
   } catch (e) {
-    return null;
+    rethrow;
   }
 }
 
-Future<String> submitForm(String email, String password) async {
-  var loginResponse =
-      await _sentFormData({'email': email, 'password': password});
+class _LoginService {
+  static submitCredentials(String email, String password) {
+    var userCredentials = {'email': email, 'password': password};
+    var client = http.Client();
+    Uri serverURL = Uri.http('localhost:8000', '/administration/login/');
 
-  /*if (loginResponse == null) {
-    return "SERVER UNREACHED";
-  }*/
+    return client.post(serverURL, body: userCredentials);
+  }
 
-  //! This will be represented in a class in future version
-  int statusCode = loginResponse.statusCode;
-  final data = json.decode(loginResponse.body);
-  var error = data["error"];
-  var state = data["state"];
-  var message = data["message"];
+  static extractResponse(response) {
+    try {
+      final int statusCode = response.statusCode;
+      final data = json.decode(response.body);
 
-  if (statusCode >= 200 && statusCode < 300) {
-    //! the logic behind this API Is to send error and state to indicate
-    //! if the request is made properly [error refers that the request in not valid]
-    //! and state will indicate the if the request made successfully
-    if (error == false && state.toUpperCase() == "FAILURE") {
-      return "FAILURE";
-    } else if (error == false && state.toUpperCase() == "SUCCESS") {
-      return "SUCCESS";
+      var error;
+      var state;
+      var message;
+      var details;
+
+      if (data["error"] != null) {
+        error = data["error"];
+      }
+
+      if (data["state"] != null) {
+        state = data["state"].toString().toUpperCase();
+      }
+
+      if (data["message"] != null) {
+        message = data["message"];
+      }
+
+      if (data["data"] != null) {
+        if (data["data"]["details"] != null) {
+          details = data["data"]["details"];
+        }
+      }
+
+      return {
+        'statusCode': statusCode,
+        'error': error,
+        'state': state,
+        'message': message,
+        'details': details
+      };
+    } catch (e) {
+      rethrow;
     }
-
-    return "UNKNOWN ERROR";
-  } else if (statusCode >= 500) {
-    return "INTERNAL SERVER ERROR";
-  } else {
-    return message; // "BAD REQUEST"
   }
 }
