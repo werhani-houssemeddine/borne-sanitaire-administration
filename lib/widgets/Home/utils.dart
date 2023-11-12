ee3927f9aa029c042d74938c65c11d550155313f
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-12 15:45:14 +0100

    home page navigation route ()

---
+++ lib/widgets/Home/utils.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: constant_identifier_names, camel_case_types
+
+//? This enum is used to controll the navbar in the desktop layout
+enum NAVIGATION_WIDGET {
+  DASHBOARD,
+  SETTINGS,
+  DEVICES,
+  CLIENTS,
+  PROFILE,
+}
