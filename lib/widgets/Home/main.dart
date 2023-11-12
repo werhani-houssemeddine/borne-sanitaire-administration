923013ce4593fcd06eeb3ef5d9749755c6e6717c
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-12 08:48:15 +0100

    Home Screen

---
+++ lib/widgets/Home/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'Layout/main.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomeLayout.init(),
+    );
+  }
+}
