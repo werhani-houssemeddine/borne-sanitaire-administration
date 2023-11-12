3ecf7d9a7a3f13d52cf557b6394d95794ef5f8b4
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-12 15:44:31 +0100

    home page layout (desktop version)

---
+++ lib/widgets/Home/Layout/desktop_layout.dart
@@ -0,0 +1,80 @@
+import 'package:borne_sanitaire_admin/widgets/Home/Widgets/nav_bar.dart';
+import 'package:flutter/material.dart';
+
+import 'package:borne_sanitaire_admin/widgets/Home/utils.dart';
+
+late NAVIGATION_WIDGET currentScreen; //! This global variable will be deleted
+
+class DesktopLayout extends StatefulWidget {
+  const DesktopLayout({super.key});
+
+  @override
+  State createState() => _DesktopLayoutState();
+}
+
+class _DesktopLayoutState extends State<DesktopLayout> {
+  @override
+  void initState() {
+    currentScreen = NAVIGATION_WIDGET.DASHBOARD;
+    super.initState();
+  }
+
+  Widget manipulateBody() {
+    if (currentScreen == NAVIGATION_WIDGET.CLIENTS) {
+      return clients;
+    } else if (currentScreen == NAVIGATION_WIDGET.DEVICES) {
+      return device;
+    } else if (currentScreen == NAVIGATION_WIDGET.SETTINGS) {
+      return settings;
+    } else if (currentScreen == NAVIGATION_WIDGET.PROFILE) {
+      return profile;
+    } else {
+      return dashboard;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    NavigateBetweenScreens screenNavigator = NavigateBetweenScreens(
+      setState: setState,
+    );
+    print("CLICKED YAL BRO $currentScreen");
+
+    return Scaffold(
+      body: Row(
+        children: [
+          NavBar(screenNavigator: screenNavigator),
+          Container(child: manipulateBody()),
+        ],
+      ),
+    );
+  }
+}
+
+Widget dashboard = const Text("DASHBOARD");
+Widget device = const Text("DEVICE");
+Widget clients = const Text("CLIENTS");
+Widget settings = const Text("SETTINGS");
+Widget profile = const Text("PROFILE");
+
+class NavigateBetweenScreens {
+  final Function setState;
+
+  NavigateBetweenScreens({required this.setState});
+
+  void _swapeBetweenScreen(NAVIGATION_WIDGET screenToNavigate) {
+    if (currentScreen != screenToNavigate) {
+      setState(() {
+        currentScreen = screenToNavigate;
+      });
+    }
+  }
+
+  get getCurrentScreen => currentScreen;
+
+  void goToDashboard() => _swapeBetweenScreen(NAVIGATION_WIDGET.DASHBOARD);
+  void goToClients() => _swapeBetweenScreen(NAVIGATION_WIDGET.CLIENTS);
+  void goToDevices() => _swapeBetweenScreen(NAVIGATION_WIDGET.DEVICES);
+  void goToProfile() => _swapeBetweenScreen(NAVIGATION_WIDGET.PROFILE);
+  void goToSettings() => _swapeBetweenScreen(NAVIGATION_WIDGET.SETTINGS);
+}
