cda5d69c3a23670337647ab4148eafdb16302515
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-13 18:33:19 +0100

    add small screen and large sreen views and start developing the large one

--- lib/widgets/Home/screens/profile.dart
+++ lib/widgets/Home/screens/profile.dart
@@ -1,3 +1,4 @@
+import 'package:borne_sanitaire_admin/widgets/Home/Widgets/profile_section.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -6,10 +7,14 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    print(size.width);
 
     return Container(
       margin: const EdgeInsets.only(bottom: 5, right: 5),
+      width: double.infinity,
+      height: double.infinity,
       decoration: _profileScreenBoxDecoration(),
+      child: size.width >= 1200 ? const LargeSreen() : const SmallSreen(),
     );
   }
 }
@@ -33,3 +38,59 @@ BoxDecoration _profileScreenBoxDecoration() {
     color: Colors.white,
   );
 }
+
+class SmallSreen extends StatelessWidget {
+  const SmallSreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProfileSection();
+  }
+}
+
+class LargeSreen extends StatelessWidget {
+  const LargeSreen({super.key});
+
+  Widget _divider() {
+    return Container(
+      width: 3,
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: Colors.grey.shade400,
+          width: 1,
+        ),
+        color: Colors.white,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const ProfileSection(),
+        _divider(),
+      ],
+    );
+  }
+}
+
+
+/*
+  class MakeProfileScreen extends StatelessWidget{
+    final Widget parentWidget; 
+    const MakeProfileScreen({super.key, required this.parentWidget });
+
+    @override
+    Widget build(BuildContext context) {
+      return parentWidget(
+        children: [
+
+        ],
+      )
+    }
+  }
+
+*/
